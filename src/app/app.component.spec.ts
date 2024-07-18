cbaae1a665864889aa8ca01570265dc4a8389d37
Anshul Sharma <anshul.sharma@alectio.com>
2024-07-18 18:36:49 +0530

    feat: Add redirects file for netlify configuration

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'assignment-anshul-sharma'`, () => {
+  it(`should have as title 'patient-doctor-dashboard'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('assignment-anshul-sharma');
+    expect(app.title).toEqual('patient-doctor-dashboard');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('assignment-anshul-sharma app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('patient-doctor-dashboard app is running!');
   });
 });
